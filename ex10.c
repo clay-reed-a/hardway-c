c4974c1ca9625995a72504b70383bbde7c35e9b0
Clayton Albachten <clay.reed.a@gmail.com>
2014-07-08 18:48:06 -0400

    ex10

--- ex10.c
+++ ex10.c
@@ -5,7 +5,7 @@ int main(int argc, char *argv[])
 	// print out each argument passed
 	int i = 0;
 
-	// argv[0] is program name 
+	// argv[0] is program name  
 	for(i = 1; i < argc; i++) 
 	{
 		printf("arg %d: %s\n",
@@ -20,7 +20,7 @@ int main(int argc, char *argv[])
 
 	int num_states = 4;
 
-	for(i = 0; i < num_states; i++) 
+	for(i = 1; i < num_states; i++) 
 	{
 		printf("state %d: %s\n", 
 			i, states[i]);
